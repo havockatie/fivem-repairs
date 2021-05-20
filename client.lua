17be953e48d9dd67cdb5d33fe78f6b496bc54907
Katie Cage <havockatie@gmail.com>
2021-05-19 20:21:18 -0400

    Initial commit.

---
+++ client.lua
@@ -0,0 +1,47 @@
+RegisterCommand("repair", function(source, args, rawCommand)
+    local playerPed = GetPlayerPed(-1)
+    local coords = GetEntityCoords(playerPed)
+    local vehicle = GetVehiclePedIsIn(playerPed, false)
+    -- local vehicle = GetClosestVehicle(coords.x, coords.y, coords.z, 5.0, 0, 71)    
+
+    local inVehicle = false
+    local isBusy = false
+
+	if vehicle == 0 then
+        vehicle = GetClosestVehicle(coords.x, coords.y, coords.z, 5.0, 0, 71)
+        if vehicle == 0 then
+            exports['mb_notify']:sendNotification('You must be inside or near a vehicle to repair it', {type="error", vertical="top", horizontal="center", variant="filled"})
+            return
+        end
+    else
+        inVehicle = true
+    end
+
+    local plate = GetVehicleNumberPlateText(vehicle)
+    local vehicleHash = GetEntityModel(vehicle)
+    local vehicleName = GetDisplayNameFromVehicleModel(vehicleHash)
+    local vehicleNameText = GetLabelText(vehicleName)
+
+    if isBusy then return end
+
+    isBusy = true
+    if inVehicle == false then
+        TaskStartScenarioInPlace(playerPed, 'PROP_HUMAN_BUM_BIN', 0, true)
+    end
+    Citizen.CreateThread(function()
+        exports['mb_notify']:sendNotification('Repairing vehicle '..plate..' '..vehicleNameText, {duration=19000, type="info", vertical="top", horizontal="center", variant="filled"})
+        Citizen.Wait(20000)
+
+        SetVehicleFixed(vehicle)
+        SetVehicleDeformationFixed(vehicle)
+        SetVehicleUndriveable(vehicle, false)
+        SetVehicleEngineOn(vehicle, true, true)
+        if inVehicle == false then
+            ClearPedTasksImmediately(playerPed)
+        end
+
+        isBusy = false
+        -- exports['mythic_notify'].DoLongHudText('success', 'Vehicle repaired')
+        exports['mb_notify']:sendNotification('Repaired vehicle '..plate..' '..vehicleNameText, {duration=5000, type="success", vertical="top", horizontal="center", variant="filled"})
+    end)
+end)
---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+
+game 'gta5' 
+
+client_scripts {
+    'client.lua'
+}
+
+name 'havoc-repairs'
+description 'A repair script. /repair'
+author 'havockatie'
